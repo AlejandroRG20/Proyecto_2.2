28b5155fa91b929608f5901efb16e17b270f5604
AlejandroRG <111157823+AlejandroRG20@users.noreply.github.com>
2023-02-25 10:46:14 -0600

    Espero el ultimo xd

---
+++ proyecto-2.2-main/Proyecto Alejandro/Views/Utilidad.Designer.cs
@@ -0,0 +1,254 @@
+﻿namespace Proyecto_Alejandro.Views
+{
+    partial class Utilidad
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.GBUB = new System.Windows.Forms.GroupBox();
+            this.Ut = new System.Windows.Forms.Label();
+            this.txtUB = new System.Windows.Forms.TextBox();
+            this.txtEgresos = new System.Windows.Forms.MaskedTextBox();
+            this.txtIngresos = new System.Windows.Forms.MaskedTextBox();
+            this.label21 = new System.Windows.Forms.Label();
+            this.label22 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.BtnLimpiar = new System.Windows.Forms.Button();
+            this.BtnAgregar = new System.Windows.Forms.Button();
+            this.label17 = new System.Windows.Forms.Label();
+            this.label20 = new System.Windows.Forms.Label();
+            this.lblHora = new System.Windows.Forms.Label();
+            this.lblFecha = new System.Windows.Forms.Label();
+            this.label19 = new System.Windows.Forms.Label();
+            this.label18 = new System.Windows.Forms.Label();
+            this.GBUB.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // GBUB
+            // 
+            this.GBUB.Controls.Add(this.Ut);
+            this.GBUB.Controls.Add(this.txtUB);
+            this.GBUB.Controls.Add(this.txtEgresos);
+            this.GBUB.Controls.Add(this.txtIngresos);
+            this.GBUB.Controls.Add(this.label21);
+            this.GBUB.Controls.Add(this.label22);
+            this.GBUB.Font = new System.Drawing.Font("Nirmala UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.GBUB.Location = new System.Drawing.Point(21, 179);
+            this.GBUB.Name = "GBUB";
+            this.GBUB.Size = new System.Drawing.Size(442, 186);
+            this.GBUB.TabIndex = 24;
+            this.GBUB.TabStop = false;
+            this.GBUB.Text = "Utilidad Bruta";
+            // 
+            // Ut
+            // 
+            this.Ut.AutoSize = true;
+            this.Ut.Location = new System.Drawing.Point(98, 117);
+            this.Ut.Name = "Ut";
+            this.Ut.Size = new System.Drawing.Size(94, 17);
+            this.Ut.TabIndex = 7;
+            this.Ut.Text = "Utilidad Bruta";
+            // 
+            // txtUB
+            // 
+            this.txtUB.Location = new System.Drawing.Point(211, 109);
+            this.txtUB.Name = "txtUB";
+            this.txtUB.Size = new System.Drawing.Size(100, 25);
+            this.txtUB.TabIndex = 6;
+            // 
+            // txtEgresos
+            // 
+            this.txtEgresos.Location = new System.Drawing.Point(294, 29);
+            this.txtEgresos.Name = "txtEgresos";
+            this.txtEgresos.Size = new System.Drawing.Size(128, 25);
+            this.txtEgresos.TabIndex = 5;
+            this.txtEgresos.ValidatingType = typeof(int);
+            // 
+            // txtIngresos
+            // 
+            this.txtIngresos.Location = new System.Drawing.Point(82, 29);
+            this.txtIngresos.Name = "txtIngresos";
+            this.txtIngresos.Size = new System.Drawing.Size(128, 25);
+            this.txtIngresos.TabIndex = 3;
+            // 
+            // label21
+            // 
+            this.label21.AutoSize = true;
+            this.label21.Location = new System.Drawing.Point(228, 37);
+            this.label21.Name = "label21";
+            this.label21.Size = new System.Drawing.Size(55, 17);
+            this.label21.TabIndex = 2;
+            this.label21.Text = "Egresos";
+            // 
+            // label22
+            // 
+            this.label22.AutoSize = true;
+            this.label22.Location = new System.Drawing.Point(16, 37);
+            this.label22.Name = "label22";
+            this.label22.Size = new System.Drawing.Size(60, 17);
+            this.label22.TabIndex = 0;
+            this.label22.Text = "Ingresos";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(325, 400);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(88, 38);
+            this.button1.TabIndex = 27;
+            this.button1.Text = "Salir";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // BtnLimpiar
+            // 
+            this.BtnLimpiar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnLimpiar.Location = new System.Drawing.Point(206, 402);
+            this.BtnLimpiar.Name = "BtnLimpiar";
+            this.BtnLimpiar.Size = new System.Drawing.Size(89, 36);
+            this.BtnLimpiar.TabIndex = 26;
+            this.BtnLimpiar.Text = "Limpiar";
+            this.BtnLimpiar.UseVisualStyleBackColor = true;
+            this.BtnLimpiar.Click += new System.EventHandler(this.BtnLimpiar_Click);
+            // 
+            // BtnAgregar
+            // 
+            this.BtnAgregar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnAgregar.Location = new System.Drawing.Point(94, 402);
+            this.BtnAgregar.Name = "BtnAgregar";
+            this.BtnAgregar.Size = new System.Drawing.Size(88, 36);
+            this.BtnAgregar.TabIndex = 25;
+            this.BtnAgregar.Text = "Agregar";
+            this.BtnAgregar.UseVisualStyleBackColor = true;
+            this.BtnAgregar.Click += new System.EventHandler(this.BtnAgregar_Click);
+            // 
+            // label17
+            // 
+            this.label17.AutoSize = true;
+            this.label17.Font = new System.Drawing.Font("Sitka Display", 21.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label17.Location = new System.Drawing.Point(116, 36);
+            this.label17.Name = "label17";
+            this.label17.Size = new System.Drawing.Size(286, 42);
+            this.label17.TabIndex = 28;
+            this.label17.Text = "FLUJO  DE  EFECTIVO";
+            // 
+            // label20
+            // 
+            this.label20.AutoSize = true;
+            this.label20.Font = new System.Drawing.Font("Nirmala UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label20.Location = new System.Drawing.Point(194, 89);
+            this.label20.Name = "label20";
+            this.label20.Size = new System.Drawing.Size(120, 17);
+            this.label20.TabIndex = 33;
+            this.label20.Text = "Empresa Josandro";
+            // 
+            // lblHora
+            // 
+            this.lblHora.AutoSize = true;
+            this.lblHora.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.lblHora.Location = new System.Drawing.Point(309, 141);
+            this.lblHora.Name = "lblHora";
+            this.lblHora.Size = new System.Drawing.Size(41, 13);
+            this.lblHora.TabIndex = 32;
+            this.lblHora.Text = "label21";
+            // 
+            // lblFecha
+            // 
+            this.lblFecha.AutoSize = true;
+            this.lblFecha.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.lblFecha.Location = new System.Drawing.Point(309, 115);
+            this.lblFecha.Name = "lblFecha";
+            this.lblFecha.Size = new System.Drawing.Size(41, 13);
+            this.lblFecha.TabIndex = 31;
+            this.lblFecha.Text = "label20";
+            // 
+            // label19
+            // 
+            this.label19.AutoSize = true;
+            this.label19.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.label19.Location = new System.Drawing.Point(161, 141);
+            this.label19.Name = "label19";
+            this.label19.Size = new System.Drawing.Size(63, 13);
+            this.label19.TabIndex = 30;
+            this.label19.Text = "Hora Actual";
+            // 
+            // label18
+            // 
+            this.label18.AutoSize = true;
+            this.label18.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.label18.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.label18.Location = new System.Drawing.Point(161, 115);
+            this.label18.Name = "label18";
+            this.label18.Size = new System.Drawing.Size(70, 13);
+            this.label18.TabIndex = 29;
+            this.label18.Text = "Fecha Actual";
+            // 
+            // Utilidad
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(485, 473);
+            this.Controls.Add(this.label20);
+            this.Controls.Add(this.lblHora);
+            this.Controls.Add(this.lblFecha);
+            this.Controls.Add(this.label19);
+            this.Controls.Add(this.label18);
+            this.Controls.Add(this.label17);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.BtnLimpiar);
+            this.Controls.Add(this.BtnAgregar);
+            this.Controls.Add(this.GBUB);
+            this.Name = "Utilidad";
+            this.Text = "Utilidad";
+            this.Load += new System.EventHandler(this.Utilidad_Load);
+            this.GBUB.ResumeLayout(false);
+            this.GBUB.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox GBUB;
+        private System.Windows.Forms.Label Ut;
+        private System.Windows.Forms.TextBox txtUB;
+        private System.Windows.Forms.MaskedTextBox txtEgresos;
+        private System.Windows.Forms.MaskedTextBox txtIngresos;
+        private System.Windows.Forms.Label label21;
+        private System.Windows.Forms.Label label22;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button BtnLimpiar;
+        private System.Windows.Forms.Button BtnAgregar;
+        private System.Windows.Forms.Label label17;
+        private System.Windows.Forms.Label label20;
+        private System.Windows.Forms.Label lblHora;
+        private System.Windows.Forms.Label lblFecha;
+        private System.Windows.Forms.Label label19;
+        private System.Windows.Forms.Label label18;
+    }
+}
---
+++ proyecto-2.2-main/Proyecto Alejandro/Views/Utilidad.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Proyecto_Alejandro.Views
+{
+    public partial class Utilidad : Form
+    {
+        public Utilidad()
+        {
+            InitializeComponent();
+        }
+
+        private void Utilidad_Load(object sender, EventArgs e)
+        {
+            txtUB.Enabled = false;
+        }
+
+        private void BtnAgregar_Click(object sender, EventArgs e)
+        {
+            float UtB;
+            float ing = float.Parse(txtIngresos.Text);
+            float egr = float.Parse(txtEgresos.Text);
+
+            if (egr <= ing)
+            {
+                UtB = ing - egr;
+                txtUB.Text = UtB.ToString("0.00");
+
+            }
+            else
+            {
+                MessageBox.Show("El egreso no puede ser mayor al ingreso");
+                txtEgresos.Text = "";
+                txtEgresos.Focus();
+            }
+        }
+
+        private void BtnLimpiar_Click(object sender, EventArgs e)
+        {
+            txtEgresos.Text = "";
+            txtIngresos.Text = "";
+            txtUB.Text = "";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
